192bcb738af04dabf9b7a636f890a734774054c3
Tram Nguyen <designertvn@gmail.com>
2024-11-05 16:50:32 -0600

    Extract Button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,17 @@
+import styles from '@/app/components/Button.module.css';
+
+export default function Button(props: {
+  id: string,
+  value: string,
+  disabled?: boolean,
+}) {
+  return (
+    <input 
+      id={props.id + "-btn"} 
+      type="button" 
+      value={props.value} 
+      disabled={props.disabled} 
+      className={styles.button}
+    />
+  );
+}
--- app/components/Controller.tsx
+++ app/components/Controller.tsx
@@ -1,27 +1,12 @@
 import styles from '@/app/components/Controller.module.css';
-
-function Button(props: {
-  id: string,
-  value: string,
-  disabled?: boolean,
-}) {
-  return (
-    <input 
-      id={props.id + "-btn"} 
-      type="button" 
-      value={props.value} 
-      disabled={props.disabled} 
-      className={styles.button}
-    />
-  );
-}
+import Button from '@/app/components/Button';
 
 export default function Controller() {
   return (
     <fieldset className={styles.controller}>
-       <Button id="shuffle" value="Shuffle"/>
-       <Button id="deselect" value="Deselect All"/>
-       <Button id="submit" value="Submit" disabled={true}/>
+       <Button id="shuffle-btn" value="Shuffle"/>
+       <Button id="deselect-btn" value="Deselect All"/>
+       <Button id="submit-btn" value="Submit" disabled={true}/>
     </fieldset>
   );
 }
