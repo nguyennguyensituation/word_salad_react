be039d8fe861c03030b40381a34bc19828a806b0
Tram Nguyen <designertvn@gmail.com>
2024-11-12 09:38:59 -0600

    Conditionally display wordle message

--- app/components/board/puzzle/Wordle.tsx
+++ app/components/board/puzzle/Wordle.tsx
@@ -4,9 +4,7 @@ import { CardState } from '@/app/lib/definitions';
 import Row from '@/app/components/board/puzzle/Row';
 
 export default function Wordle(props: { card: CardState }) {
-  const { word } = props.card;
-  const [letters] = useState<string[]>(new Array(word.length).fill(''));
-  const [message] = useState<string>('test message');
+  const [message] = useState<string>('');
   const [rows] = useState(new Array(6).fill(''));
 
   return (
@@ -17,7 +15,9 @@ export default function Wordle(props: { card: CardState }) {
         })}
       </section>
       <section>
-        <p className={styles.message}>{message}</p>
+        {message && 
+          <p className={styles.message}>{message}</p>
+        }
       </section>
     </>
   );
