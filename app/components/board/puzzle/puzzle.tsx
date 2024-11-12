855a872367597c72ebd3bd18601f492a3c1700fb
Tram Nguyen <designertvn@gmail.com>
2024-11-12 12:33:44 -0600

    Refactor Puzzle component, extract confirmClose function

--- app/components/board/puzzle/puzzle.tsx
+++ app/components/board/puzzle/puzzle.tsx
@@ -4,40 +4,39 @@ import Wordle from '@/app/components/board/puzzle/Wordle';
 import { CardState } from '@/app/lib/definitions';
 import { PUZZLE_INSTRUCTIONS, PUZZLE_MESSAGES } from '@/app/lib/messages';
 
+function confirmClose(card: CardState, closePuzzle: (card: CardState) => void) {
+  const {puzzlePlayed, puzzleType} = card;
+  if (!puzzlePlayed && puzzleType && confirm(PUZZLE_MESSAGES[puzzleType])) {
+    closePuzzle(card);
+  }
+}
+
 export default function Puzzle(props: {
   card: CardState,
   closePuzzle: (card: CardState) => void,
 }) {
-  const card = props.card;
-  const closePuzzle = props.closePuzzle;
+  const {card, closePuzzle} = props;
   const puzzleType = card.puzzleType;
   const puzzleClasses = `${styles.title} ${puzzleType === 'crossword' ? 'blue' : 'green'}`;
-
-  function confirmClose() {
-    if (!card.puzzlePlayed) {
-      if (puzzleType && confirm(PUZZLE_MESSAGES[puzzleType])) {
-        closePuzzle(card);
-      }
-    }
-  }
 
   return (
     <article className={styles.puzzle}>
       <header>
         <section>
           <h2 className={puzzleClasses}>{puzzleType}</h2>
-          <button className={styles.closeBtn} onClick={() => confirmClose()}>
+          <button className={styles.closeBtn} onClick={() => {
+            confirmClose(card, closePuzzle);
+          }}>
             &#215;
           </button>
         </section>
         {puzzleType &&
           <p className={styles.instructions}>
-            <b>How to play: </b> {PUZZLE_INSTRUCTIONS[puzzleType]}
+            <b>How to play: </b>{PUZZLE_INSTRUCTIONS[puzzleType]}
           </p>
         }
       </header>
-      {puzzleType === 'crossword' && <Crossword card={card}/>}
-      {puzzleType === 'wordle' && <Wordle card={card}/>}
+      {puzzleType === 'crossword' ? <Crossword card={card}/> : <Wordle card={card}/>}
     </article>
   );
 }
