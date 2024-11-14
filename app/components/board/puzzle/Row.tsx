766d0124ca1a5aab762db8316a8d1fa353398689
Tram Nguyen <designertvn@gmail.com>
2024-11-14 10:13:50 -0600

    Wordle Row components are comprised of Cell components and populate values from the rows state var

--- app/components/board/puzzle/Row.tsx
+++ app/components/board/puzzle/Row.tsx
@@ -1,16 +1,15 @@
 import styles from '@/app/components/board/puzzle/Row.module.css';
 
-export default function Row(props: {
-  row: string[],
-  idx: number,
-}) {
+function Cell(props: {key: string, ltr: string}) {
+  return <div className={styles.cell}>{props.ltr}</div>;
+}
+
+export default function Row(props: {row: string[], idx: number}) {
   return (
-    <div className={styles.row} key={props.idx}>
-      <div className={styles.tile}></div>
-      <div className={styles.tile}></div>
-      <div className={styles.tile}></div>
-      <div className={styles.tile}></div>
-      <div className={styles.tile}></div>
+    <div className={styles.row} key={`row-${props.idx}`}>
+      {props.row.map((ltr, rowIdx) => {
+        return <Cell key={`${props.idx}-${rowIdx}`} ltr={ltr}/>;
+      })}
     </div>
   );
 }
--- app/components/board/puzzle/Wordle.tsx
+++ app/components/board/puzzle/Wordle.tsx
@@ -5,22 +5,18 @@ import Row from '@/app/components/board/puzzle/Row';
 
 export default function Wordle(props: { card: CardState }) {
   const [message] = useState<string>('');
-  const [rows] = useState(new Array(6).fill(''));
+  const [rows] = useState(new Array(6).fill(['', '', '', '', '']));
 
   return (
     <>
       <section className={styles.wordleContainer}>
         {rows.map((row, idx) => {
-          return <Row row={row} idx={idx} key={idx}/>;
+          return <Row row={row} idx={idx} key={`row-${idx}`}/>;
         })}
       </section>
       <section>
-        {message &&
-          <p className={styles.message}>{message}</p>
-        }
+        {message && <p className={styles.message}>{message}</p>}
       </section>
     </>
   );
 }
-
-
